1ef6e52591744be07dcba1df1c83f914f5187d18
Phil Davis <phil@jankaritech.com>
2017-06-28 13:48:03 +0545

    Need the require in the right place

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,13 +23,13 @@ if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
+require_once __DIR__.'/../../../lib/base.php';
+
 $unitTestLocation = '/tests/unit/';
 
-if (!file_exists(\OC::$SERVERROOT . $unitTestLocation)) {
+if (!file_exists(OC::$SERVERROOT . $unitTestLocation)) {
 	$unitTestLocation = '/tests/lib/';
 }
-
-require_once __DIR__.'/../../../lib/base.php';
 
 \OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . $unitTestLocation, true);
 
