7b6ab92f1c6b2c23c1d04905e2b6b4a9ee0fef9d
ownClouders <devops@owncloud.com>
2019-07-30 05:54:26 +0000

    [tx] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -30,6 +30,7 @@ OC.L10N.register(
     "_* and %n more _::_* and %n more _" : ["* et %n autre","* et %n autres"],
     "You are receiving this email because the following things happened at <a href=\"%s\">%s</a>" : "Vous recevez ce courriel car l'activité suivante a eu lieu sur <a href=\"%s\">%s</a>",
     "%1$s - %2$s" : "%1$s - %2$s",
+    "_and %n more _::_and %n more _" : ["et %n supplémentaire","et %n supplémentaires"],
     "Activity" : "Activité",
     "List your own file actions in the stream" : "Liste de vos propres actions dans le flux",
     "Notify about your own actions via email" : "M'informer de mes propres actions par courriel",
