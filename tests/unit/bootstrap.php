6b17117ab87ece1590f686a99d1c4f5c1ea0eab0
Phil Davis <phil@jankaritech.com>
2022-05-26 15:37:31 +0545

    Remove unneeded items from tests/unit/bootstrap.php

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -5,10 +5,6 @@ if (!\defined('PHPUNIT_RUN')) {
 
 require_once __DIR__ . '/../../../../lib/base.php';
 
-// especially with code coverage it will require some more time
-\set_time_limit(0);
-
 \OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
-\OC::$composerAutoloader->addPsr4('TestHelpers\\', OC::$SERVERROOT . '/tests/TestHelpers/', true);
 // load activity unit test classes
 \OC::$composerAutoloader->addPsr4('OCA\\Activity\\Tests\\Unit\\', __DIR__, true);
